d92c28b17f1e667f55eb49254587efeee8b372c3
liufeibiao <15311414320@163.com>
2024-08-05 11:15:05 +0800

    重载

--- study/test_overload.py
+++ study/test_overload.py
@@ -1,18 +1,41 @@
-from typing import (
-    Any,
-    Callable,
-    ContextManager,
-    Iterator,
-    List,
-    Literal,
-    NamedTuple,
-    Optional,
-    overload,
-    Sequence,
-    Tuple,
-    TypeVar,
-    Union,
-)
+# from typing import overload
+#
+#
+# @overload
+# def test_a(a: int, ) -> tuple: ...
+#
+#
+# # @overload
+# # def test_a(a: str, b: dict, ) -> list: ...
+#
+#
+# def test_a(a, b, ):
+# 	print(type(a), a, type(b), b)
+#
+#
+# test_a("ss", {})
+from typing import overload, Union, Dict, List, Tuple
+
+
+# 重载签名
+@overload
+def test_a(a: int) -> Tuple[int]: ...
+
 
 @overload
-def _cudnn_ctc_loss(log_probs: Tensor, targets: Tensor, input_lengths: Tensor, target_lengths: Tensor, blank: _int, deterministic: _bool, zero_infinity: _bool) -> Tuple[Tensor, Tensor]: ...
+def test_a(a: str, b: Dict) -> List[str]: ...
+
+
+# 实际实现
+def test_a(a: Union[int, str], b: Dict = None) -> Union[List[str], Tuple[int]]:
+	if isinstance(a, int):
+		return (a,)
+	elif isinstance(a, str) and isinstance(b, dict):
+		return [a, b]
+	else:
+		raise TypeError("Invalid arguments")
+
+
+# 调用实际实现
+print(test_a(10))  # 输出: (10,) .直接调用tuple的函数
+print(test_a("ss", {}))  # 输出: ['ss',{}] .直接调用list的函数
